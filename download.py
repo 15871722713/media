fb6b00b8ef922d447ace3c5f6ccfeef3ac79a6e9
JinHua <15871722713@163.com>
2020-04-15 17:09:54 +0800

    First commit

---
+++ download.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : JinHua
+# @Date    : 2020-04-14 16:50:40
+# @Contact : hgu_autotesta103@fiberhome.com
+# @Site    :
+# @File    : download.py
+# @Software: PyCharm
+# @Desc    :
+# @license : Copyright(C), cienet
---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : JinHua
+# @Date    : 2020-04-14 16:50:13
+# @Contact : hgu_autotesta103@fiberhome.com
+# @Site    :
+# @File    : main.py
+# @Software: PyCharm
+# @Desc    :
+# @license : Copyright(C), cienet
